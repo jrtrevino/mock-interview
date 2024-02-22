733262d3d4685e661bacf5a1f5a8e2ff77286818
jrtrevino <j.trevino95@icloud.com>
2024-02-21 18:07:02 -0800

    add problem 1

---
+++ 1346-lc/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+        def checkIfExist(self, arr: List[int]) -> bool:
+            return False
+
+if __name__ == "__main__":
+    input1 = [10,2,5,3]
+    input2 = [3,1,7,11]
+    solution = Solution()
+    response1 = solution.checkIfExist(input1)
+    response2 = solution.checkIfExist(input2)
+
+    assert response1 == True
+    assert response2 == False
